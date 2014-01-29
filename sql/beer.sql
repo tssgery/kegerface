b33ff937b7fc2df6ce32f45e8d9975b9527606fa
Eric Young <eric@aceshome.com>
2014-01-29 22:02:26 +0000

    modifying to use IBU numbers instead of subjective hop index

--- sql/beer.sql
+++ sql/beer.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 29, 2014 at 10:00 PM
+-- Generation Time: Jan 29, 2014 at 10:02 PM
 -- Server version: 5.5.35
 -- PHP Version: 5.4.4-14+deb7u7
 
@@ -53,76 +53,6 @@ INSERT INTO `Beer` (`Tap`, `Beer`, `Brewery`, `Style`, `ABV`, `IBU`, `SRM`, `Des
 (0, 'Corona', 'Corona', 'Mexican Lager', '4.6 %', 1, 5, ''),
 (0, 'Corona Light', 'Corona', 'Mexican Lager', '3.7 %', 0, 5, ''),
 (1, 'Fat Tire', 'New Belgium', 'Amber Ale', '5.2%', 22, 10, 'Toasty malt, gentle sweetness, flash of fresh hop bitterness. The malt and hops are perfectly balanced.');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `BeerStock`
---
-
-CREATE TABLE IF NOT EXISTS `BeerStock` (
-  `ID` int(10) NOT NULL AUTO_INCREMENT,
-  `Beer` varchar(255) COLLATE latin1_german2_ci NOT NULL,
-  `Quantity` int(11) NOT NULL,
-  `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Reason` varchar(255) COLLATE latin1_german2_ci NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `Beer` (`Beer`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=97 ;
-
---
--- Dumping data for table `BeerStock`
---
-
-INSERT INTO `BeerStock` (`ID`, `Beer`, `Quantity`, `Date`, `Reason`) VALUES
-(10, 'Coronita', 3570, '2013-04-14 15:35:45', 'Stock Update'),
-(11, 'Bittersweet Apple Cider', 1000, '2013-04-14 15:35:45', 'Stock'),
-(12, 'Carlsberg', 8360, '2013-04-14 15:35:45', 'Stock'),
-(13, 'Double Chocolate Stout', 500, '2013-04-14 15:35:45', 'Stock'),
-(14, 'Sole Star', 3500, '2013-04-14 15:35:45', 'Stock'),
-(15, 'Stella 4', 0, '2013-04-14 15:35:45', 'Stock'),
-(17, 'Toffee Apple Cider', 0, '2013-04-14 15:35:45', 'Stock'),
-(18, 'Winter Zest', 4620, '2013-04-14 15:35:45', 'Stock'),
-(24, 'Vintage Reserve', 0, '2013-04-14 15:35:45', 'Stock Update'),
-(19, 'Alcohol Free', 1925, '2013-04-14 15:35:45', 'Stock'),
-(20, 'Farm Pressed Medium Cider', 500, '2013-04-14 15:35:45', 'Stock'),
-(21, '2011 Vintage Cider', 500, '0000-00-00 00:00:00', 'Stock'),
-(22, 'Crabbies', 330, '2013-04-14 15:35:45', 'Stock'),
-(23, 'Mythos', 0, '2013-04-14 15:35:45', 'Stock'),
-(25, 'Mocha Beer', 500, '2013-04-14 15:35:45', 'Stock Update'),
-(26, 'Pumpking', 500, '2013-04-14 15:35:45', 'Stock Update'),
-(27, 'Rudolph', 500, '2013-04-14 15:35:45', 'Stock Update'),
-(32, 'Ginger Zest', 2310, '2013-04-14 15:39:52', 'Stock'),
-(30, 'Black Cherry Cider', 568, '2013-04-14 15:35:45', 'Stock'),
-(31, 'Peach & Apricot Cider', 0, '2013-04-14 15:35:45', 'Stock'),
-(33, 'Summer Zest', 1980, '2013-04-16 11:41:42', 'Stock'),
-(96, 'Magners', 7040, '2013-05-04 11:06:39', 'Stock'),
-(95, 'Carling Cider', 0, '2013-05-04 11:06:06', 'Stock'),
-(94, 'Spindrift', 0, '2013-05-04 11:06:06', 'Stock'),
-(93, 'Brooklyn Lager', 355, '2013-04-28 10:27:53', 'Stock'),
-(92, 'Boston Lager', 660, '2013-04-28 10:27:39', 'Stock'),
-(91, 'Jaipur IPA', 500, '2013-04-28 10:27:39', 'Stock');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `readings`
---
-
-CREATE TABLE IF NOT EXISTS `readings` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date_time` datetime DEFAULT NULL,
-  `temperature` float DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `readings`
---
-
-INSERT INTO `readings` (`id`, `date_time`, `temperature`) VALUES
-(1, '2013-04-15 11:09:41', 21),
-(2, '2013-04-15 11:09:49', 21.5);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
